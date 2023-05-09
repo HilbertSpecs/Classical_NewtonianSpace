c4efe123e1e1c8b73549a625d08301df3424b030
HilbertSpecs <43871228+HilbertSpecs@users.noreply.github.com>
2023-05-08 23:22:27 -0700

    Remove Compiled Object files and Bash Build Script

--- exeElementaryAnalysis.sh
+++ exeElementaryAnalysis.sh
@@ -1,5 +1,5 @@
 g++ NewtonianSpace.hpp
-g++ -c -o NewtonianSpace2.o NewtonianSpace2.cpp
+g++ -c -o NewtonianSpace.o NewtonianSpace.cpp
 g++ -c -o ElementaryAnalysis.o ElementaryAnalysis.cpp
 g++ -o exeElementaryAnalysis NewtonianSpace2.o ElementaryAnalysis.o
 ./exeElementaryAnalysis
