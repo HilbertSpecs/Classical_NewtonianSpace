bd93874a609b8eb54f3cdd0ee83df771ca3b2041
HilbertSpecs <43871228+HilbertSpecs@users.noreply.github.com>
2023-05-09 00:14:03 -0700

    Remove original build script

--- exeElementaryAnalysis.sh
+++
@@ -1,5 +0,0 @@
-g++ NewtonianSpace.hpp
-g++ -c -o NewtonianSpace.o NewtonianSpace.cpp
-g++ -c -o ElementaryAnalysis.o ElementaryAnalysis.cpp
-g++ -o exeElementaryAnalysis NewtonianSpace.o ElementaryAnalysis.o
-./exeElementaryAnalysis
